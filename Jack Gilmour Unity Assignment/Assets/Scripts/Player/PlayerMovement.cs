10fd033e1d4a4f72f07a342626e17ef1499e39c1
JackGilmour <s189701@uos.ac.uk>
2018-11-19 14:27:11 +0000

    Started
    Started creating the basic elements for my Scream Beam game by creating movement scripts and  attacking scripts

---
+++ Jack Gilmour Unity Assignment/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float HorizontalMovement;
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void fixedupdate () horizontal
+    {
+        float
+
+    }
